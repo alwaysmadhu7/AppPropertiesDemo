d3fa191d53caf9a0c25168c946a466eeb95551a6
Madhu <Madhu@192.168.0.6>
2019-01-24 22:17:04 +0530

    Added JUnit cases

--- src/test/java/com/example/demo/AppPropertiesDemoApplicationTests.java
+++ src/test/java/com/example/demo/AppPropertiesDemoApplicationTests.java
@@ -1,16 +1,34 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.appprop.demo.MyCompanyPropertiesMap;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppPropertiesDemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testForMap() {
+		MyCompanyPropertiesMap companyPropertiesMap=new MyCompanyPropertiesMap();
+		Map<String,String> map =new HashMap<>();
+		map.put("key1", "value1@gmail.com");
+		map.put("key2", "value2@gmail.com");
+		companyPropertiesMap.setMap(map);
+		Assert.assertEquals(companyPropertiesMap, new MyCompanyPropertiesMap().getMap());
+	}
+	
+	@Test
+	public void testForBoolean() {
+		Assert.assertTrue(true);
 	}
 
 }
